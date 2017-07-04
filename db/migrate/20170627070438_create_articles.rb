74f585a20c47310860a7d6b2826e85de111e608d
t.palevela <t.palevela@manuhindia.com>
2017-07-04 18:25:32 +0530

    added

--- db/migrate/20170627070438_create_articles.rb
+++ db/migrate/20170627070438_create_articles.rb
@@ -1,10 +1,10 @@
 class CreateArticles < ActiveRecord::Migration[5.0]
   def change
     create_table :articles do |t|
-      t.string :book_name
-      t.date   :published
+      t.string :name
+      t.boolean   :published, :default=> false
       t.text   :description
-      t.string   :author 
+      t.integer :editor_id
       t.timestamps
     end
   end
--- db/migrate/20170629100024_change_columns_article.rb
+++ db/migrate/20170629100024_change_columns_article.rb
@@ -2,7 +2,8 @@ class ChangeColumnsArticle < ActiveRecord::Migration[5.0]
   def change
   	rename_column :articles, :book_name, :name
   	rename_column :articles, :author, :editor_id
-  	change_column :articles, :editor_id, :integer
+	  remove_column :articles, :editor_id
+  	add_column :articles, :editor_id, :integer
   	change_column :articles, :published, :boolean, :default=> false
 
 
